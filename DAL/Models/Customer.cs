0d7e6ce000cadd87fee7adb3fe171448aecf77b7
alexovonathan22 <avo.nathan@gmail.com>
2020-02-25 13:25:52 +0100

    first commit

---
+++ DAL/Models/Customer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace DAL.Models
+{
+    public class Customer : AccountViewModel
+    {
+        [Required]
+        [Display(Name = "Name")]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        public string NewCustomerName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+
+        [Required]
+        [Display(Name = "Address")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        public string Address { get; set; }
+
+        [Required]
+        [Display(Name = "Contact Number")]
+        [StringLength(11, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 11)]
+        public string ContactNumber { get; set; }
+    }
+}
